8d67189be54d9e55f35d8e578e82da53cde1d7c4
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-03 23:12:39 +0000

    add imports

---
+++ aleveltransitworkshop.py
@@ -0,0 +1,10 @@
+#Imports
+import streamlit as st
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import time
+# use the non-interactive Agg backend to be more thread safe
+#mpl.use("agg")
+from matplotlib.backends.backend_agg import RendererAgg
+_lock = RendererAgg.lock

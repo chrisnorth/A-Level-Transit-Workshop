9e94164d63d29663198f60af128526aae745ef9c
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-03 23:41:22 +0000

    corrected question

--- aleveltransitworkshop.py
+++ aleveltransitworkshop.py
@@ -37,9 +37,22 @@ if section==1:
     # Add an image title
     st.title('K2-18 b')
     # Add an image from local file
-    st.image('C:\Users\elena\Pictures\Desktop background\Exoplanet_K2-18_b_(Illustration).jpg', caption='The above image is an illustration of an exoplanet Kb-18 b! The red sphere is the cool dwarf star that it orbits around called K2-18. Illustration: NASA, CSA, ESA, J. Olmsted (STScI), Science: N. Madhusudhan (Cambridge University)')
+    #st.image('C:\Users\elena\Pictures\Desktop background\Exoplanet_K2-18_b_(Illustration).jpg', caption='The above image is an illustration of an exoplanet Kb-18 b! The red sphere is the cool dwarf star that it orbits around called K2-18. Illustration: NASA, CSA, ESA, J. Olmsted (STScI), Science: N. Madhusudhan (Cambridge University)')
+    # Define the question and options
+    question1_1_1 = "What is the transit method used for?"
+    options1_1_1 = ["Detecting exoplanets", "Detecting comets", "Studying asteroids", "Observing galaxies"]
+    # Display the question and options
+    selected_option = st.radio(question1_1_1, options1_1_1)
+    # Check the selected option
+    if selected_option == "Detecting exoplanets":
+        st.write("Correct! The transit method is used for detecting exoplanets. Mission One complete!")
+    else:
+        st.write("Try again! Re-read the Mission One details!")
+        # Provide a hint
+        if selected_option == "Detecting comets":
+            st.write("Try again! Re-read the Mission One details.")
+        elif selected_option == "Studying asteroids":
+            st.write("Try again! Re-read the Mission One details.")
+        elif selected_option == "Observing galaxies":
+            st.write("Try again! Re-read the Mission One details.")
 
-    import streamlit as st
-
-    # Question 1
-    options_q1 = ["Detecting exoplanets", "Detecting comets", "Observing galaxies", "Rome"]

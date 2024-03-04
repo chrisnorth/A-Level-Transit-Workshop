bb32dc853a4554d13622d202481fd51c66817000
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-04 00:40:11 +0000

    Update aleveltransitworkshop.py

--- aleveltransitworkshop.py
+++ aleveltransitworkshop.py
@@ -35,6 +35,10 @@ section = st.radio('Select mission:', [1,2], format_func=sectiontitle)
 st.markdown("## {}".format(sectiontitle(section)))
 
 if section==1:
+    # Load the image from a local file
+    image = open("C:\Users\elena\Pictures\Desktop background\Exoplanet_K2-18_b_(Illustration).jpg", "rb").read()
+    # Display the image in Streamlit
+st.image(image, caption='The above image is an illustration of an exoplanet Kb-18 b! The red sphere is the cool dwarf star that it orbits around called K2-18. Illustration: NASA, CSA, ESA, J. Olmsted (STScI), Science: N. Madhusudhan (Cambridge University)')
     # Add text
     st.write("The transit method is a way astronomers detect exoplanets, which are planets outside of our solar system.")
     import streamlit as st
@@ -125,7 +129,7 @@ if section==2:
         #Convert the animation to HTML format using to_jshtml() method of the FuncAnimation object.
         #html = ani.to_html5_video()
         # Display the animation in Streamlit
-        components.html(ani.to_jshtml())
+        components.html(ani.to_jshtml(), height=1000)
     st.markdown("Imagine you're standing far away and watching a distant star. Now, if a planet passes in front of that star from your perspective, you will see a tiny shadow. This is the planet blocking some of the star's light! Have a look at the plot below of a planet going around a star.")
     st.markdown("Astronomers can detect this because they see a small dip in the star's brightness. By carefully observing these dips in brightness over time, astronomers can figure out if there might be a planet orbiting that star. They can also learn about the size of the planet, how long it takes to orbit its star, and sometimes even its atmosphere!")
     st.markdown("Ready to proceed to the next level? Answer the questions below!")

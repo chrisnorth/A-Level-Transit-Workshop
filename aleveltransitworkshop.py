4169dad60fec90e4d51e121a7d09eb127983a87a
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-04 00:35:44 +0000

    correcting animation in st

--- aleveltransitworkshop.py
+++ aleveltransitworkshop.py
@@ -1,5 +1,6 @@
 #Imports
 import streamlit as st
+import streamlit.components.v1 as components
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -61,6 +62,9 @@ if section==1:
             st.write("Try again! Re-read the Mission One details.")
 
 if section==2:
+    # Add progress bar in sidebar
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empt
     # Parameters
     star_radius = 1.0
     planet_radius = 0.3
@@ -121,7 +125,7 @@ if section==2:
         #Convert the animation to HTML format using to_jshtml() method of the FuncAnimation object.
         #html = ani.to_html5_video()
         # Display the animation in Streamlit
-        st.video(ani, format='video/mp4', start_time=0)
+        components.html(ani.uo_jshtml())
     st.markdown("Imagine you're standing far away and watching a distant star. Now, if a planet passes in front of that star from your perspective, you will see a tiny shadow. This is the planet blocking some of the star's light! Have a look at the plot below of a planet going around a star.")
     st.markdown("Astronomers can detect this because they see a small dip in the star's brightness. By carefully observing these dips in brightness over time, astronomers can figure out if there might be a planet orbiting that star. They can also learn about the size of the planet, how long it takes to orbit its star, and sometimes even its atmosphere!")
     st.markdown("Ready to proceed to the next level? Answer the questions below!")

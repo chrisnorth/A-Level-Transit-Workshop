8d013e337a5c03370c7eb67a6330bec7a6fa52b3
Elena-Davies <DaviesEM19@cardiff.ac.uk>
2024-03-04 00:09:20 +0000

    update

--- aleveltransitworkshop.py
+++ aleveltransitworkshop.py
@@ -8,6 +8,8 @@ import time
 #mpl.use("agg")
 from matplotlib.backends.backend_agg import RendererAgg
 _lock = RendererAgg.lock
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML
 
 # Title the app
 st.title('A-Level Transit Method: The Transit Trail!')
@@ -15,6 +17,7 @@ st.title('A-Level Transit Method: The Transit Trail!')
 # adding pages
 st.markdown("# Level One: Uncovering The Transit Method")
 st.sidebar.markdown("# Level One")
+st.write("Level One: clear, smooth terrain with 10% chance of rain!")
 
 # Define section titles
 sectiontitles = ['Mission One', 'Mission Two']
@@ -31,7 +34,6 @@ st.markdown("## {}".format(sectiontitle(section)))
 
 if section==1:
     # Add text
-    st.write("Level One: clear, smooth terrain with 10% chance of rain!")
     st.write("The transit method is a way astronomers detect exoplanets, which are planets outside of our solar system.")
     import streamlit as st
     # Add an image title
@@ -47,7 +49,7 @@ if section==1:
     selected_option = st.text_input("Type the number (0-3) corresponding to your solution:")
     # Check the selected option
     if selected_option == "0":
-        st.write("Correct! The transit method is used for detecting exoplanets. Mission One complete!")
+        st.write("Correct! The transit method is used for detecting exoplanets. Mission One complete! Provide to your next Mission :)")
     else:
         # Provide a hint
         if selected_option == "1":
@@ -57,3 +59,13 @@ if section==1:
         elif selected_option == "3":
             st.write("Try again! Re-read the Mission One details.")
 
+if section==2:
+    # Parameters
+    star_radius = 1.0
+    planet_radius = 0.3
+    orbit_radius = 3.0
+    angular_velocity = 0.02
+    frames = 400
+    st.markdown("Imagine you're standing far away and watching a distant star. Now, if a planet passes in front of that star from your perspective, you will see a tiny shadow. This is the planet blocking some of the star's light! Have a look at the plot below of a planet going around a star.")
+    st.markdown("Astronomers can detect this because they see a small dip in the star's brightness. By carefully observing these dips in brightness over time, astronomers can figure out if there might be a planet orbiting that star. They can also learn about the size of the planet, how long it takes to orbit its star, and sometimes even its atmosphere!")
+    st.markdown("Ready to proceed to the next level? Answer the questions below!")

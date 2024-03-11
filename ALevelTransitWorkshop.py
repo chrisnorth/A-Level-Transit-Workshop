4a98c45d024139762fcdf18323102afe8532f7bf
Chris North <chris.north@astro.cf.ac.uk>
2024-03-11 09:56:55 +0000

    add streamlit version print

--- ALevelTransitWorkshop.py
+++ ALevelTransitWorkshop.py
@@ -4,6 +4,7 @@ import streamlit.components.v1 as components
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+# import PyTransit
 import time
 # use the non-interactive Agg backend to be more thread safe
 #mpl.use("agg")
@@ -13,8 +14,8 @@ from matplotlib.animation import FuncAnimation
 
 # Title the app
 st.title('A-Level Transit Method: The Transit Trail!')
-
 # adding pages
 st.write("# Welcome to The Transit Trail!")
 st.sidebar.success("Select a level above.")
-    
+
+st.write('streamlit version',st.__version__)

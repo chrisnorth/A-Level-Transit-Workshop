c24038ee1ab85c31b477a5eaa752a5d6df1439f5
Chris North <chris.north@astro.cf.ac.uk>
2024-03-18 10:37:43 +0000

    remove extra line

--- pages/2_Level_Two.py
+++ pages/2_Level_Two.py
@@ -64,9 +64,6 @@ rp_rs116b = rp116b/rs18 # planet radius (Kepler-116 b) / stellar radius ratio
 tm = QuadraticModel() # a model that uses two limb-darkening coefficients
 tm.set_data(t)
 
-    
-tm = QuadraticModel() # a model that uses two limb-darkening coefficients
-
 if section2==1:
     # Add text
     st.write('When the planet moves in front of the star, the light of the star dims slightly for a short period of time. Think about when you put your hand in front of a torch and the light gets dimmer! Now, imagine this but the torch is the size of the Sun and your hand is the size of the Earth and both are thousands of kilometres away! This is very hard to see so astronomers have to use sensitive instruments that monitor the brightness of stars and analyse plots called transit light curves to see the dips in brightness. Have a look at the transit light curve of a real exoplanet called K2-18 b below!')

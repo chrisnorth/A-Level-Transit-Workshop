58e7ee59732a15e9a04b865172dcb44e90bf9ee6
Chris North <chris.north@astro.cf.ac.uk>
2024-03-18 10:23:28 +0000

    use agg

--- pages/1_Level_One.py
+++ pages/1_Level_One.py
@@ -6,9 +6,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import time
 # use the non-interactive Agg backend to be more thread safe
-#mpl.use("agg")
+mpl.use("agg")
 from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
+#_lock = RendererAgg.lock
 from matplotlib.animation import FuncAnimation
 
 #Imports
